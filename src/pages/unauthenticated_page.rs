003daad9d411a4e5c69ea7c20dba5c04bd8a5ee1
meraghon <mdayton0521@gmail.com>
2025-09-26 13:37:49 -0400

    Fixed issue where LoginLink was spanning the container

--- src/pages/unauthenticated_page.rs
+++ src/pages/unauthenticated_page.rs
@@ -8,9 +8,11 @@ pub fn UnauthenticatedPage() -> impl IntoView {
         <div class="flex h-screen justify-center items-center pb-50">
             <div class="bg-white p-8 border rounded-lg shadow-lg">
                 <h3> "You're not signed in. Please sign in with your Region 15 email." </h3>
-                <LoginLink class="text-login flex pt-4 justify-center items-center">
-                    <ActionButton> "Sign in" </ActionButton>
-                </LoginLink>
+                <div class="flex pt-4 justify-center items-center">
+                    <LoginLink class="text-login">
+                        <ActionButton> "Sign in" </ActionButton>
+                    </LoginLink>
+                </div>
             </div>
         </div>
 
