d5659f6da76fa5ad8aab1ea7a9ad6452058b8fd1
meraghon <mdayton0521@gmail.com>
2025-12-05 11:59:30 -0500

    Added to mod.rs

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,6 +2,7 @@ mod action_button;
 mod checkbox;
 mod chips;
 mod dashboard_button;
+mod date;
 mod loading;
 mod multi_entry;
 mod panel;
@@ -12,6 +13,6 @@ mod text_field;
 mod utils;
 
 pub use self::{
-    action_button::*, checkbox::*, chips::*, dashboard_button::*, loading::*, multi_entry::*,
-    panel::*, radio::*, row::*, select::*, text_field::*,
+    action_button::*, checkbox::*, chips::*, dashboard_button::*, date::*, loading::*,
+    multi_entry::*, panel::*, radio::*, row::*, select::*, text_field::*,
 };
