03f7adb74ddf97c970bf0107b3a6585af4eb0601
meraghon <mdayton0521@gmail.com>
2025-12-22 10:36:07 -0500

    Created DateStatus enum

--- src/common/date_info.rs
+++ src/common/date_info.rs
@@ -8,10 +8,19 @@ pub struct DateInfo {
     pub(crate) date: DateRange,
     #[serde(rename = "desc")]
     pub(crate) description: String,
+    #[serde(rename = "stat")]
+    pub(crate) status: DateStatus,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum DateRange {
     Single(String),
     Range(String, String),
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DateStatus {
+    Upcoming,
+    Open,
+    Deadline,
+    Closed,
 }
