9b4e4cbe1bcee7def145913983f85069b3585d5c
meraghon <mdayton0521@gmail.com>
2026-01-07 15:39:56 -0500

    Removed stuff

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,22 +1,17 @@
 mod action_button;
 mod banner;
-mod checkbox;
-mod chips;
 mod dashboard_button;
 mod file_upload;
+mod lists;
 mod loading;
 mod multi_entry;
 mod panel;
-mod radio;
 mod row;
-mod select;
 mod text_field;
 mod utils;
 mod validated_form;
-mod lists;
 
 pub use self::{
-    action_button::*, banner::*, dashboard_button::*, file_upload::*, lists::*,
-    loading::*, multi_entry::*, panel::*, row::*, text_field::*,
-    validated_form::*
+    action_button::*, banner::*, dashboard_button::*, file_upload::*, lists::*, loading::*,
+    multi_entry::*, panel::*, row::*, text_field::*, validated_form::*,
 };
