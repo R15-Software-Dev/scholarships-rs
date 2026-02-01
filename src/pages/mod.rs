75f77d5ddf3f3727fafc9f9972d908ebb73bb48b
Lucas Vas <lvas463@gmail.com>
2026-02-01 12:11:27 -0500

    chore: remove unused test page

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -5,7 +5,6 @@ mod home_page;
 mod loaner_page;
 mod provider_portal;
 mod scholarship_info;
-mod test_page;
 mod unauthenticated_page;
 mod provider_contact;
 mod auth_callback;
@@ -15,6 +14,6 @@ mod student;
 
 pub use self::{
     about_page::*, comparison_test::*, home_page::*, loaner_page::*, provider_portal::*,
-    test_page::*, unauthenticated_page::*, scholarship_info::*, provider_contact::*,
+    unauthenticated_page::*, scholarship_info::*, provider_contact::*,
     auth_callback::*, provider_applicants::*, admin::*, student::*,
 };
