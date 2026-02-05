38994088dfc833db02132509034397d48aefd9d6
Lucas Vas <lvas463@gmail.com>
2026-02-05 10:52:19 -0500

    chore: recenter after recent changes

--- src/pages/unauthenticated_page.rs
+++ src/pages/unauthenticated_page.rs
@@ -32,7 +32,7 @@ pub fn UnauthenticatedPage() -> impl IntoView {
     };
 
     view! {
-        <div class="flex h-screen justify-center items-center pb-50">
+        <div class="flex mx-auto h-screen justify-center items-center pb-50">
             <div class="bg-white p-8 border rounded-lg shadow-lg">
                 <h3>"You're not signed in. Please sign in first."</h3>
                 <div class="flex pt-4 justify-center items-center">
