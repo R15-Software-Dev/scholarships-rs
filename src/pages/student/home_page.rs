80340d99b91a975c7d24932e0609fdcbb17f44aa
Lucas Vas <lvas463@gmail.com>
2026-03-06 01:11:07 -0500

    feat: add redirect buttons to forms

--- src/pages/student/home_page.rs
+++ src/pages/student/home_page.rs
@@ -1,55 +1,36 @@
-use std::collections::HashMap;
-use leptos::logging::log;
 use leptos::prelude::*;
-use crate::common::ValueType;
-use crate::components::ActionButton;
-use crate::pages::api::students::{GetStudentData, PutStudentData};
-use crate::utils::get_user_claims;
+use crate::components::DashboardButton;
 
 #[component]
 pub fn StudentHomePage() -> impl IntoView {
-    let user_claims = get_user_claims();
-    let user_id = Memo::new(move |_| {
-        user_claims.get()
-            .map(|info| info.claims.subject.clone())
-    });
-    
-    let create_student_data = ServerAction::<PutStudentData>::new();
-    let get_student_data = ServerAction::<GetStudentData>::new();
-    
-    let on_click_student_test = move |_| {
-        create_student_data.dispatch(PutStudentData {
-            subject: user_id.get().unwrap_or_default(),
-            data_type: "TESTING".into(),
-            data_map: HashMap::from([
-                ("testing_info".into(), ValueType::String(Some("hello!".to_string())))
-            ]),
-        });
-    };
-    
-    let on_click_get_student = move |_| {
-        get_student_data.dispatch(GetStudentData {
-            subject: user_id.get().unwrap_or_default(),
-            data_type: "TESTING".into()
-        });
-    };
-    
-    Effect::new(move || {
-        let Some(value) = get_student_data.value().get() else {
-            return;
-        };
-        
-        log!("{:?}", value);
-    });
-    
     view! {
-        <div class="flex flex-1 flex-col gap-4 items-center m-4">
-            <div class="font-bold text-3xl">"Student Home Page"</div>
+        <div class="flex-1" />
+        <div class="flex flex-2 flex-col gap-4 items-center m-4 mt-10">
+            <div class="font-bold text-3xl">"Region 15 Scholarship Application"</div>
             <div class="text-lg">
-                "This is just a placeholder for now. We'll show some redirect buttons."
+                "Welcome to the R15 general scholarship application! If you have any questions, please direct them to Sara Smith at "
+                <a class="text-blue-500 underline" href="mailto:ssmith@region15.org">"ssmith@region15.org"</a>"."
             </div>
-            <ActionButton on:click=on_click_student_test>"Test Put Student Function"</ActionButton>
-            <ActionButton on:click=on_click_get_student>"Test Get Student Function"</ActionButton>
+            <div class="text-lg">"Here are some links to get you started:"</div>
+            <DashboardButton
+                title="Scholarship Book"
+                description="View scholarship requirements"
+                icon="/Book.png"
+                path="https://docs.google.com/document/d/1qVs0Lwvwl1bVGHznocoev3AF56KlhTfs/view"
+            />
+            <DashboardButton
+                title="Demographics Forms"
+                description="Basic student information"
+                icon="/Person_Black.png"
+                path="/students/demographics"
+            />
+            <DashboardButton
+                title="Additional Forms"
+                description="Extra eligibility factors"
+                icon="/Form_Black.png"
+                path="/students/demographics"
+            />
         </div>
+        <div class="flex-1" />
     }
 }
